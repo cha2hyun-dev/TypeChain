b993c14104aa0823a60971e08581fe7f47a1eb76
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-11-17 16:03:48 +0900

    #0.7 Classes on Typescript part one (interface vs class)

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "soohyun",
-    age: 22,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("lynn", 20, "female");
 const sayHi = (person) => {
     return `hello ${person.name} ${person.age} ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name : string,
-    age : number,
-    gender : string
+class Human {
+    public name : string;
+    public age : number;
+    public gender : string;
+    constructor(name: string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name : "soohyun",
-    age : 22,
-    gender : "male"
-};
+const lynn =new Human("lynn",20,"female")
 
 const sayHi = (person : Human):string => {
     return `hello ${person.name} ${person.age} ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
